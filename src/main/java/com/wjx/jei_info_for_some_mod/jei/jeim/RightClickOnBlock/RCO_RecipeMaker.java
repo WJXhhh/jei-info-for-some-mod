4847fd3eb0e57920fdb2bd833a42af48ae870843
WJXhhh <1928692778@qq.com>
2022-07-25 16:02:00 +0800

    v-22 为机器加合成表

--- src/main/java/com/wjx/jei_info_for_some_mod/jei/jeim/RightClickOnBlock/RCO_RecipeMaker.java
+++ src/main/java/com/wjx/jei_info_for_some_mod/jei/jeim/RightClickOnBlock/RCO_RecipeMaker.java
@@ -2,11 +2,15 @@ package com.wjx.jei_info_for_some_mod.jei.jeim.RightClickOnBlock;
 
 import com.google.common.collect.Lists;
 import mezz.jei.api.IJeiHelpers;
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Loader;
+import net.tslat.aoa3.block.functional.utility.CrystalCreator;
 import net.tslat.aoa3.common.registration.BlockRegister;
+import net.tslat.aoa3.common.registration.BlockRegister.*;
 import net.tslat.aoa3.common.registration.ItemRegister;
+import net.tslat.aoa3.common.registration.ItemRegister.*;
 
 import java.util.List;
 
@@ -23,19 +27,25 @@ public class RCO_RecipeMaker {
 
             jeiRecipes.add(new RCO_Recipe(l1,new ItemStack(ItemRegister.RED_CRYSTAL)));
             jeiRecipes.add(new RCO_Recipe(l2,new ItemStack(ItemRegister.YELLOW_CRYSTAL)));
-            jeiRecipes.add(C(ItemRegister.PURPLE_GEMSTONES, Item.getItemFromBlock(BlockRegister.PURPLE_CRYSTAL_CREATOR),ItemRegister.PURPLE_CRYSTAL));
-            jeiRecipes.add(C(ItemRegister.BLUE_GEMSTONES,Item.getItemFromBlock(BlockRegister.BLUE_CRYSTAL_CREATOR),ItemRegister.BLUE_CRYSTAL));
-            jeiRecipes.add(C(ItemRegister.WHITE_GEMSTONES,Item.getItemFromBlock(BlockRegister.WHITE_CRYSTAL_CREATOR),ItemRegister.WHITE_CRYSTAL));
+            jeiRecipes.add(C(ItemRegister.PURPLE_GEMSTONES,BlockRegister.PURPLE_CRYSTAL_CREATOR,ItemRegister.PURPLE_CRYSTAL));
+            jeiRecipes.add(C(ItemRegister.BLUE_GEMSTONES,BlockRegister.BLUE_CRYSTAL_CREATOR,ItemRegister.BLUE_CRYSTAL));
+            jeiRecipes.add(C(ItemRegister.WHITE_GEMSTONES,BlockRegister.WHITE_CRYSTAL_CREATOR,ItemRegister.WHITE_CRYSTAL));
+            jeiRecipes.add(C(ItemRegister.GREEN_GEMSTONES,BlockRegister.GREEN_CRYSTAL_CREATOR,ItemRegister.GREEN_CRYSTAL));
         }
 
 
         return jeiRecipes;
     }
 
-    public static RCO_Recipe C(Item input1,Item input2,Item output){
+
+
+    /**
+     * RCO中的C是适用于快速制作出一个RCO_Recipe的函数
+     *
+     */
+    public static RCO_Recipe C(Item input1, Block input2, Item output){
         List<ItemStack> lc = Lists.newArrayList(new ItemStack(input1),new ItemStack(input2));
         return new RCO_Recipe(lc,new ItemStack(output));
-
     }
 
 
